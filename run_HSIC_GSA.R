9b286d91b779f37bdb059c01c262813e7213354d
rohmerj <j.rohmer@brgm.fr>
2026-01-30 15:56:42 +0100

    Add files via upload

--- run_HSIC_GSA.R
+++ run_HSIC_GSA.R
@@ -9,9 +9,16 @@ NOM = "GujanMestras"
 load(paste0(NOM,"-","DomingosT2.82m.RData"))
 
 ireseau = 3 ## lead time number ireseau (higher means small lead time)
+#load(file=paste0("./resu/PE_GpCond/",NOM,"-",tempete,"-ireseau",ireseau,"_MC100.RData"))
 print(ireseau)
-load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_MC100.RData"))
 
+load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_DOE.RData"))
+load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_part1.RData"))
+load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_part2.RData"))
+load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_part3.RData"))
+load(file=paste0("./",NOM,"-",tempete,"-ireseau",ireseau,"_part4.RData"))
+
+'
 ## formating of inputs
 OU = which(RR[,2] == 2)
 RR = RR[OU,]
@@ -19,8 +26,27 @@ RR = RR[,-2]
 
 ## formating of the output (takes some time)
 hat = NULL
-for (imc in 1:nrow(RR)){
+HE.HAT_part2 = list()
+for (imc in 1:1050){
 	hat0 = abs(HE.HAT[[OU[imc]]][1,]-truth) ## absolute difference with the reference solution
+	hat = rbind(hat,hat0)
+}
+'
+hat = NULL
+for (imc in 1:250){
+	hat0 = abs(HE.HAT_part1[[imc]][1,]-truth) ## absolute difference with the reference solution
+	hat = rbind(hat,hat0)
+}
+for (imc in 251:500){
+	hat0 = abs(HE.HAT_part2[[imc-250]][1,]-truth) ## absolute difference with the reference solution
+	hat = rbind(hat,hat0)
+}
+for (imc in 501:750){
+	hat0 = abs(HE.HAT_part3[[imc-500]][1,]-truth) ## absolute difference with the reference solution
+	hat = rbind(hat,hat0)
+}
+for (imc in 751:1050){
+	hat0 = abs(HE.HAT_part4[[imc-750]][1,]-truth) ## absolute difference with the reference solution
 	hat = rbind(hat,hat0)
 }
 
